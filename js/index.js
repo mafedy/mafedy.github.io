52d4e5e0525424c528c10654390b491c42b04ffd
mafedy <82313421+mafedy@users.noreply.github.com>
2021-10-12 19:46:29 +0200

    v2
    Test Version

--- js/index.js
+++ js/index.js
@@ -31,12 +31,3 @@ $(document).ready(function(){
     } // End if
   });
 });
-
-
-//POP UP PDF
-function openWin(url){
-newwin=window.open(url,'','width=400,height=400,top=200,left=200');
-if(newwin){
-window.onfocus=function(){newwin.window.close()}
-}
-}
